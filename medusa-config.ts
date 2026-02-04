f135a2a9f7bf784495291f88e2bb095939a1c48b
curtdk <942233@qq.com>
2026-02-04 23:05:46 +0800

    加入 meilisearch

--- medusa-config.ts
+++ medusa-config.ts
@@ -12,5 +12,60 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  }
+  },
+  plugins: [
+    {
+      resolve: "@rokmohar/medusa-plugin-meilisearch",
+      options: {
+        config: {
+          host: process.env.MEILISEARCH_HOST,
+          apiKey: process.env.MEILISEARCH_API_KEY,
+        },
+        settings: {
+          products: {
+            type: "products",
+            enabled: true,
+            fields: [
+              "id",
+              "title",
+              "description",
+              "handle",
+              "variant_sku",
+              "thumbnail",
+              "status",
+              "created_at",
+              "updated_at",
+            ],
+            indexSettings: {
+              searchableAttributes: [
+                "title",
+                "description",
+                "variant_sku",
+                "handle",
+              ],
+              displayedAttributes: [
+                "id",
+                "handle",
+                "title",
+                "description",
+                "variant_sku",
+                "thumbnail",
+                "status",
+              ],
+              filterableAttributes: [
+                "id",
+                "handle",
+                "status",
+              ],
+              sortableAttributes: [
+                "created_at",
+                "updated_at",
+              ],
+            },
+            primaryKey: "id",
+          },
+        },
+      },
+    },
+  ],
 })
